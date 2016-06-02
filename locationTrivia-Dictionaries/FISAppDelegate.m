0d7a436f180c8e3500093ac36bf74f7cad005165
Erica <erica@Ericas-MacBook-Air.local>
2016-06-02 09:07:26 -0400

    pull rqst

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -16,4 +16,61 @@
  
  */
 
+- (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+
+    NSString *name = location[@"name"];
+    if ( [name length] > length ) {
+        name = [name substringToIndex:length];
+        return name;
+    } else {
+        return name;
+    }
+}
+
+- (NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    NSMutableDictionary *nDictionary = [[NSMutableDictionary alloc] init];
+    [nDictionary setValue:name forKey:@"name"];
+    [nDictionary setValue:@(latitude) forKey:@"latitude"];
+    [nDictionary setValue:@(longitude) forKey:@"longitude"];
+    return nDictionary;
+}
+
+- (NSArray *)namesOfLocations:(NSArray *)locations {
+    NSMutableArray *arryOfLocations = [[NSMutableArray alloc] init];
+    for (NSDictionary *location in locations) {
+        NSString *nameOfLocation = location[@"name"];
+        [arryOfLocations addObject:nameOfLocation];
+    }
+    return arryOfLocations;
+}
+
+- (BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary {
+    NSArray *keys = [dictionary allKeys];
+    if ([keys count] != 3) {
+        return false;
+    } else if ([keys containsObject: @"name"] && [keys containsObject: @"latitude"] && [keys containsObject: @"longitude"]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+- (NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations {
+    for (NSDictionary *location in locations) {
+        if ([location[@"name"] isEqual:name]) {
+            return location;
+        }
+    }
+    return nil;
+}
+
+
+
+
+
+
+
+
+
+
 @end
